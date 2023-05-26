24ef3bb7f2ce9c36fc2500e21c7f3dda5cc0e6b7
Fox <johnafoxwell@gmail.com>
2023-05-25 20:18:38 -0400

    Heroku first upload

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 // Use the index file in public folder for homepage
 app.use(express.static('public'));
 
+// Get route for home page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // Listen
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
